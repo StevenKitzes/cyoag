9da769cbbae4ce998cba7bb9feb14bd89ac28696
Steven Kitzes <steven.kitzes@parivedasolutions.com>
2016-07-31 21:35:10 -0700

    gh-pages v.1.1.0, added first batch of user stories and made User Stories section collapsable

--- docs/js/functional-requirements.js
+++ docs/js/functional-requirements.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
   $('#iterations-container').toggle();
   $('#definitions-container').toggle();
+  $('#user-stories-container').toggle();
 
   $('#iterations-expander').click(function() {
     $('#iterations-container').slideToggle();
   });
   $('#definitions-expander').click(function() {
     $('#definitions-container').slideToggle();
+  });
+  $('#user-stories-expander').click(function() {
+    $('#user-stories-container').slideToggle();
   });
 });
