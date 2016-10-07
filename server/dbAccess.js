0d2a54cce8f190c35e2b394c76ab027a929ff9ca
Steven Kitzes <steven.kitzes@gmail.com>
2016-10-07 00:12:41 -0700

    Working database connection and prototype query, added a UID generator

--- server/dbAccess.js
+++ server/dbAccess.js
@@ -1,11 +1,16 @@
+// Require the MySQL library for NodeJS
 var mysql = require('mysql');
 
+// This is a connection pool, which will be made available to this app's routes
 var pool;
 
+// This is the function we'll actually expose via module.exports; it returns
+//    a reference to this module's SQL database connection pool
 function db() {
   if(pool == null || pool == undefined) {
-    console.log('Setting up database.');
+    console.log('Connection pool did not exist yet.  Setting it up.');
 
+    // Create the connection pool if it didn't exist yet, using mostly defaults
     pool = mysql.createPool({
       connectionLimit: 10,
       host: 'localhost',
@@ -15,7 +20,9 @@ function db() {
     });
   }
 
+  // Return ref to the pool we created, or if it already existed, return it
   return pool;
 }
 
-module.exports = this.db;
+// Expose the function that returns a reference to the connection pool
+module.exports = db;
--- server/routes/testPost.js
+++ server/routes/testPost.js
@@ -1,16 +1,40 @@
+// Relevant require's . . .
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// Warn anyone trying to acces this endpoint via a GET request
 router.get('/', function(req, res, next) {
   res.send('This is a POST method; you have arrived here by mistake.<br><br>...or were you trying to be a sneak?');
 });
+
+// POST request, the kind we want at this endpoint!
 router.post('/', function(req, res, next) {
+  // Ghetto simulation of user authentication
   if(req.body.foo == 'bar') {
-    res.send('Got content!  Hooray!');
+    // Get a reference to our SQL database connection pool
+    var pool = require('../dbAccess')();
+
+    // Use pool to query ... callback runs on result, success or fail
+    pool.query('SELECT * FROM users;', function(err, rows, fields) {
+      // In case of query error
+      if(err) {
+        res.send(err);
+        return;
+      }
+      // In case of successful query with no results
+      if(rows.length < 1) {
+        res.send('Got no rows back.  :(');
+        return;
+      }
+      // In case of successful query
+      res.send('Got ' + rows.length + ' rows back!');
+      return;
+    });
   } else {
+    // Handle failed ghetto authentication placeholder
     res.send('Got unexpected content.');
   }
 });
 
+// Expose router for Express
 module.exports = router;
