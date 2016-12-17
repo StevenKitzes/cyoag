b50244e9761c9b0761f0685945f804066cdf49a5
Steven Kitzes <steven.kitzes@gmail.com>
2016-12-17 00:19:43 -0800

    Updated to improve usage of letsencrypt certs

--- server/build-config.js
+++ server/build-config.js
@@ -12,20 +12,23 @@ switch(config.env) {
   case constants.envLocal:
     config.hostDomain = constants.hostDomainLocal;
     config.httpPort = 3000;
-    config.httpsCertKeyLocation = secrets.httpsCertKeyLocation_local;
-    config.httpsCertLocation = secrets.httpsCertLocation_local;
+    config.httpsPrivKeyLocation = secrets.httpsPrivKeyLocation_local;
+    config.httpsFullChainLocation = secrets.httpsFullChainLocation_local;
+    config.httpsChainLocation = secrets.httpsChainLocation_local;
     break;
   case constants.envProd:
     config.hostDomain = constants.hostDomainProd;
     config.httpPort = 80;
-    config.httpsCertKeyLocation = secrets.httpsCertKeyLocation;
-    config.httpsCertLocation = secrets.httpsCertLocation;
+    config.httpsPrivKeyLocation = secrets.httpsPrivKeyLocation;
+    config.httpsFullChainLocation = secrets.httpsFullChainLocation;
+    config.httpsChainLocation = secrets.httpsChainLocation;
     break;
   default:
     config.hostDomain = constants.hostDomainLocal;
     config.httpPort = 3000;
-    config.httpsCertKeyLocation = secrets.httpsCertKeyLocation_local;
-    config.httpsCertLocation = secrets.httpsCertLocation_local;
+    config.httpsPrivKeyLocation = secrets.httpsPrivKeyLocation_local;
+    config.httpsFullChainLocation = secrets.httpsFullChainLocation_local;
+    config.httpsChainLocation = secrets.httpsChainLocation_local;
     break;
 }
 
