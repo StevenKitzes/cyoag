f0e7bacbee3ffd5e1995ea4034fe1ce4718b3445
Steven Kitzes <steven.kitzes@gmail.com>
2016-12-22 21:42:58 -0800

    Fix to follow React build rules and improve build configs in general

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,8 +1,26 @@
 var webpack = require('webpack');
 var path = require('path');
 
+var constants = require('./constants');
+var cyoagBuildConfig = require('./build-config');
+
 var BUILD_DIR = path.resolve(__dirname, 'public/js');
 var APP_DIR = path.resolve(__dirname, 'build-source/js');
+
+var plugins;
+if(cyoagBuildConfig.env == constants.envProd) {
+  plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ];
+}
+else {
+  plugins = [];
+}
 
 var config = {
   entry: APP_DIR + '\\main.js',
@@ -18,7 +36,8 @@ var config = {
         loader: 'babel'
       }
     ]
-  }
+  },
+  plugins: plugins
 };
 
 module.exports = config;
