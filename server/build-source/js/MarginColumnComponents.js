275f09681588dd81427aad25fb76060711904917
root <root@CYOAG-ubuntu16.04-512mb-sfo2-01>
2017-01-02 07:26:38 +0000

    Further verbiage updates to Privacy Policy teaser

--- server/build-source/js/MarginColumnComponents.js
+++ server/build-source/js/MarginColumnComponents.js
@@ -40,7 +40,7 @@ var MarginLogin = React.createClass({
         <label id='cyoag-tos-label'>Agree to <a className='cyoag-link' href='tos.html'>Terms and Conditions</a>: <input id='cyoag-tos-checkbox' type="checkbox"></input></label>
         <a href='priv-pol.html'><div id='cyoag-social-note-container'>
           <img id='cyoag-info-badge' src='images/info-gray.png' />
-          <div id='cyoag-social-note-column'><p id='cyoag-social-note' className='cyoag-note-green'>Learn how and why CYOAG connects with your social media account in the CYOAG Privacy Policy!</p></div>
+          <div id='cyoag-social-note-column'><p id='cyoag-social-note' className='cyoag-note-green'>Learn about social media account integration in the CYOAG Privacy Policy!</p></div>
         </div></a>
       </div>
     );
